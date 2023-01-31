9e5758fc8f802cb47f3c706f2870dc0e0ae5ee9a
powder-rose <glittervodka@yandex.ru>
2023-01-31 16:36:11 +0300

    form fixed

--- scripts.js
+++ scripts.js
@@ -1,46 +1,87 @@
-let password = document.getElementById('#password')
-let passwordRepeat = document.getElementById('#passwordRepeat')
-let errors = []
+let username = document.getElementById('username')
+let surname = document.getElementById('surname')
+let login = document.getElementById('login')
+let password = document.getElementById('password')
+let passwordRepeat = document.getElementById('passwordRepeat')
+let tel = document.getElementById('telNumber')
+let submit = document.querySelector('.registry-form__btn')
 
+let input = document.querySelector('.registry-form__input')
+let errorName = document.querySelector('.errorName')
+let errorSurname = document.querySelector('.errorSurname')
+let errorLogin = document.querySelector('.errorLogin')
+let errorPassoword = document.querySelector('.passwordError')
+let errorPasswordRepeat = document.querySelector('.passwordRepeatError') 
+let errorTel = document.querySelector('.telError')
 
-
-const validateForm = (input) => {
-   let validity = input.validity
-   input.min = 10
-   input.max = 20
-
+function checkName () {
+   let validity = username.validity
    if (validity.valueMissing) {
-      errors.push(`Поле '${input.placeholder.toLowerCase()}' не заполнено`)
+      errorName.innerHTML = 'Укажите ваше имя'
+   } else {
+      errorName.innerHTML = ''
+     
    }
-   if (validity.patternMismatch) {
-      errors.push(`Неверный формат заполнения поля ${input.placeholder.toLowerCase()}`)
-   }
-   if (validity.rangeOverflow == false ){
-      errors.push(`Количество символов не может превышать ${input.min}`)
-   }
-   if (validity.rangeUnderflow == false) {
-      errors.push(`Количество символов не может быть меньше чем ${input.min}`)
-   }
-   
-
-   // if (password.value != passwordRepeat.value) {
-   //    errors.push('Пароли не совпадают')
-   // }
    
 }
 
-
-const checkAll = () => {
-   errors = []
-   let inputs = document.querySelectorAll('.registry-form__input')
-   for (let input of inputs) {
-      validateForm(input)
+function checkSurname () {
+   let validity = surname.validity
+   if (validity.valueMissing) {
+  errorSurname.innerHTML = 'Укажите фамилию'
+   } else {
+      errorSurname.innerHTML = ''
+      
    }
    
-   document.getElementById('err').innerHTML = errors.join(' <br>')
 }
 
-  
-   let btn = document.getElementById('submit')
-   btn.addEventListener('click', checkAll)
+function checkLogin () {
+   let validity = login.validity
+   if (validity.valueMissing) {
+      errorLogin.innerHTML = 'Укажите логин'
+   } else if (validity.patternMismatch) {
+      errorLogin.innerHTML = 'Неверный формат заполнения поля'
+   } else {
+      errorLogin.innerHTML = ''
+     
+   }
+   
+}
 
+function checkPassword () {
+   let validity = password.validity
+   if (validity.valueMissing) {
+      errorPassoword.innerHTML = 'Укажите пароль'
+   } else if (password.value !== passwordRepeat.value) {
+      errorPasswordRepeat.innerHTML = 'Пароли не совпадают'
+   }  else if (validity.tooShort) {
+      errorPassoword.innerHTML = 'Длина пароля не должна быть меньше 8 символов'
+   } else if (validity.patternMismatch) {
+      errorPassoword.innerHTML = 'Пароль может содержать только латинские буквы или цифры'
+   } else { 
+      errorPassoword.innerHTML = ''
+      errorPasswordRepeat.innerHTML = ''
+     
+   }
+   
+}
+
+function checkTel () {
+   let validity = tel.validity
+   if (validity.valueMissing) {
+      errorTel.innerHTML = 'Укажите номер телефона'
+   } else if (validity.patternMismatch) {
+      errorTel.innerHTML = 'Неверный формат заполнения номера телефона'
+   } else {
+      errorTel.innerHTML = ''
+     
+   }
+   
+}
+
+submit.addEventListener('click', checkName)
+submit.addEventListener('click', checkSurname)
+submit.addEventListener('click', checkLogin)
+submit.addEventListener('click', checkPassword)
+submit.addEventListener('click', checkTel)
